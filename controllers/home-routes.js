cb12750eff7d3e0b5551f2b398a2d002a12d5c7f
Garlic Fiend <suzannaakins@gmail.com>
2020-11-06 11:42:44 -0800

    comment form, footer styling

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -45,18 +45,44 @@ router.get('/login', (req, res) => {
 });
 
 router.get('/post/:id', (req, res) => {
-    const post = {
-        id: 1,
-        content: 'blah blah blah blogs',
-        title: 'single post page test',
-        created_at: new Date(),
-        comments: [{}, {}],
-        user: {
-            username: 'test_user'
-        }
-    };
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'title',
+            'content',
+            'created_at'
+        ],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            },
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            }
+        ]
+    })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: 'No post found with this id' });
+                return;
+            }
 
-    res.render('single-post', { post });
+            const post = dbPostData.get({ plain: true });
+            res.render('single-post', { post });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 module.exports = router;
