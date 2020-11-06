45cc62fe777d2e0c45d3c74ec5ad2b8de3deaf89
Garlic Fiend <suzannaakins@gmail.com>
2020-11-05 23:35:22 -0800

    post routes added and tested

--- models/index.js
+++ models/index.js
@@ -3,4 +3,13 @@
 const User = require('./User');
 const Post = require('./Post');
 
+//create ONE TO MANY relationship between user and posts :
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
 module.exports = { User, Post };
