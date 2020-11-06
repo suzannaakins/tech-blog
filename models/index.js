b5817d5c5841797eb4d96588f4e77a1123a92001
Garlic Fiend <suzannaakins@gmail.com>
2020-11-06 00:00:40 -0800

    comment routes including edit a comment

--- models/index.js
+++ models/index.js
@@ -2,6 +2,7 @@
 
 const User = require('./User');
 const Post = require('./Post');
+const Comment = require('./Comment');
 
 //create ONE TO MANY relationship between user and posts :
 User.hasMany(Post, {
@@ -12,4 +13,21 @@ Post.belongsTo(User, {
     foreignKey: 'user_id',
 });
 
-module.exports = { User, Post };
+// association between comments and its post, and comments and its user
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Comment };
