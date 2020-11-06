e353eab884afdaaf61897b0421e0ed814a667fc4
Garlic Fiend <suzannaakins@gmail.com>
2020-11-06 11:20:23 -0800

    nav styling

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -101,6 +101,18 @@ router.post('/login', (req, res) => {
     });
 });
 
+//LOGOUT users
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    }
+    else {
+        res.status(404).end();
+    }
+});
+
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
     // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
