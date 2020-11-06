b4b1ab079a2427f6170617fdfbb08b43a2a939a3
Garlic Fiend <suzannaakins@gmail.com>
2020-11-06 15:27:58 -0800

    user can edit

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -3,6 +3,7 @@ async function editFormHandler(event) {
 
     const id = window.location.toString().split('/')[window.location.toString().split('/').length - 1];
     const title = document.querySelector('input[name="post-title"]').value;
+    const content = document.querySelector('input[name="post-content"]').value;
 
     const response = await fetch(`/api/posts/${id}`, {
         method: 'PUT',
