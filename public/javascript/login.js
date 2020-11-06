f61803c23bd31972847100db174e5bc4119441c5
Garlic Fiend <suzannaakins@gmail.com>
2020-11-06 14:08:28 -0800

    header styling

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -38,7 +38,7 @@ async function loginFormHandler(event) {
         });
 
         if (response.ok) {
-            document.location.replace('/');
+            document.location.replace('/dashboard');
         } else {
             alert(response.statusText);
         }
