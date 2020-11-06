22ad928b899d4c2fb197f4f09cbfd5f2334896fd
Garlic Fiend <suzannaakins@gmail.com>
2020-11-06 15:18:00 -0800

    edit post button takes you to edit page with form

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -41,4 +41,52 @@ router.get('/', withAuth, (req, res) => {
         });
 });
 
+
+router.get('/edit/:id', withAuth, (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'content',
+            'title',
+            'created_at',
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: 'No post found with this id' });
+                return;
+            }
+
+            // serialize the data
+            const post = dbPostData.get({ plain: true });
+
+            // pass data to template
+            res.render('edit-post', {
+                post,
+                loggedIn: req.session.loggedIn
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
 module.exports = router;
